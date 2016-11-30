5fbbaadca4ad3f9dea1dc301d090e4143ff91ade
paulo <pj.martins@campus.fct.unl.pt>
2016-11-30 17:05:29 +0000

    Users can now only see internal info of another user only if they are friends

--- src/main/java/servlets/User.java
+++ src/main/java/servlets/User.java
@@ -3,14 +3,17 @@ package main.java.servlets;
 import main.java.app.Authenticator;
 import main.java.exceptions.UndefinedAccountException;
 import main.java.model.Account;
+import main.java.model.AccountClass;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.logging.Logger;
 
 @WebServlet(name = "User", urlPatterns = { "/User/*"} )
@@ -25,22 +28,48 @@ public class User extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        String username = request.getPathInfo();
+        HttpSession session = request.getSession(false);
+        String username = ((AccountClass) session.getAttribute("USER")).getUsername();
 
-        if(username == null || username.length() == 1) {
+        String profileName = request.getPathInfo();
+
+        if(profileName == null || profileName.length() == 1) {
             response.sendRedirect("/Friends");
             return;
         }
-        username = username.substring(1);
+        profileName = profileName.substring(1);
+
+        boolean isFriend = false;
+        List<String> friendList = null;
 
         try {
-            Account acc = auth.get_account(username);
+            friendList = auth.get_friends(username);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        for (String friend: friendList) {
+            if (friend.equals(profileName)) {
+                isFriend = true;
+                break;
+            }
+        }
+
+        try {
+            Account acc = auth.get_account(profileName);
             if(acc.getLocked() == 1) {
                 response.sendRedirect("/Friends");
                 return;
             }
-            String userInfo = acc.getUsername() + "<br/>" + acc.getEmail() + "<br/>" + acc.getPhone() + "<br/>"
-                    + acc.getBio() + "<br/>" + acc.getSecretInfo();
+            String userInfo;
+
+
+            if (isFriend)
+                userInfo = acc.getUsername() + "<br/>" + acc.getEmail() + "<br/>" + acc.getPhone() + "<br/>"
+                    + acc.getBio() + "<br/>";
+            else
+                userInfo = acc.getUsername() + "<br/>" + acc.getEmail() + "<br/>" + acc.getPhone() + "<br/>";
+
             request.setAttribute("msg", userInfo);
         } catch (SQLException | UndefinedAccountException e) {
             request.setAttribute("errorMessage", e.getMessage());
